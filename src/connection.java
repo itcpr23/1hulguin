7419e888e200abac79a82bbe64fcf33f476b9ed7
hp <hp@Traci>
2019-09-08 17:58:35 +0800

    init commit

---
+++ src/connection.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class connection {
+    protected String dbname = "juvs";
+    protected String url = "jdbc:mysql://localhost/"+dbname;
+    protected String username = "root";
+    protected String password = ""; 
+    
+}
